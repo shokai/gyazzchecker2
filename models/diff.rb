05a43f5735a4a047cba23b6a07bf925e0f04afa5
Sho Hashimoto <hashimoto@shokai.org>
2012-06-02 13:57:36 +0900

    delete models/diff

--- models/diff.rb
+++
@@ -1,7 +0,0 @@
-class Diff
-  include Mongoid::Document
-  field :gyazz_name, :type => String, :default => ''
-  field :page_name, :type => String, :default => ''
-  field :lines, :type => Array, :default => Array.new
-  field :created_at, :type => Time, :default => lambda{ Time.now }
-end
